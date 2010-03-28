dce912299b678f1f9093cb1bdd325b5a4499597e
Simon Rozet <simon@rozet.name>
2010-03-28 16:48:57 -0700

    allow to specify channel password

--- main.js
+++ main.js
@@ -5,13 +5,15 @@ process.addListener('SIGINT', function() {
   sys.puts("Got a SIGINT");
   process.exit();
 });
+
 var listen_port = process.ARGV[2];
 var username    = process.ARGV[3];
 var channel     = process.ARGV[4];
+var password    = process.ARGV[5];
 
 if(listen_port != null && username != null && channel != null) {
-  new irccat.server(channel, username, listen_port);
+  new irccat.server(channel, password, username, listen_port);
 } else {
   var executable = [process.ARGV[0], process.ARGV[1]].join(' ')
-  sys.puts(executable + " port username channel");
+  sys.puts(executable + " port username channel [password]");
 }
